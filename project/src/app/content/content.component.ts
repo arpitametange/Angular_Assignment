98ac17ecfde1eaf384a14a702c90e208df244bb7
arpitametange <arpitametange@gmail.com>
2023-10-01 18:11:35 +0530

    added some styles on ui side

--- project/src/app/content/content.component.ts
+++ project/src/app/content/content.component.ts
@@ -1,20 +1,22 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { read, utils } from 'xlsx';
+import { Component, OnInit } from '@angular/core';
+import { read, utils, WorkBook, WorkSheet } from 'xlsx';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
   // Side nav related variables
   searchName: string = '';
   selectedJobTitles: string[] = [];
   selectedEmployees: string[] = [];
   selectedCompanies: string[] = [];
   data: any;
-
+  companySize: Set<string> = new Set<string>();  //for the options
+  companytitle: Set<string> = new Set<string>(); 
+  jobname: Set<string> = new Set<string>(); 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
@@ -24,14 +26,22 @@ export class ContentComponent {
       .subscribe((data: any) => {
         const datas = new Uint8Array(data); // Convert ArrayBuffer to Uint8Array
         // Parsing the Excel data
-        const workbook = read(datas, { type: 'array' });
-        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+        const workbook: WorkBook = read(datas, { type: 'array' });
+        const worksheet: WorkSheet = workbook.Sheets[workbook.SheetNames[0]];
         this.data = utils.sheet_to_json(worksheet); // Convert worksheet to JSON
         console.log(this.data); // Log the fetched data for debugging
-      });
+        
+        // Collect unique company sizes
+        this.data.forEach((item: any) => {
+          this.companySize.add(item.CompanySize);
+          this.jobname.add(item.JobTitles);
+          this.companytitle.add(item.CompanyName);
+        });
+  
+  
+      })
+  
   }
-
-
   // Method to remove Job Title chip
   removeJobTitle(jobTitle: string): void {
     const index = this.selectedJobTitles.indexOf(jobTitle);
@@ -40,7 +50,7 @@ export class ContentComponent {
       this.selectedJobTitles = [...this.selectedJobTitles]; // Update ngModel to trigger change detection
     }
   }
-  
+
   removeEmployee(employee: string): void {
     const index = this.selectedEmployees.indexOf(employee);
     if (index >= 0) {
@@ -48,7 +58,7 @@ export class ContentComponent {
       this.selectedEmployees = [...this.selectedEmployees]; // Update ngModel to trigger change detection
     }
   }
-  
+
   removeCompanyName(companyName: string): void {
     const index = this.selectedCompanies.indexOf(companyName);
     if (index >= 0) {
@@ -56,22 +66,19 @@ export class ContentComponent {
       this.selectedCompanies = [...this.selectedCompanies]; // Update ngModel to trigger change detection
     }
   }
-  
 
- // Method to get the count of selected job titles
- getSelectedJobTitlesCount(): number {
-  return this.selectedJobTitles.length;
-}
+  // Method to get the count of selected job titles
+  getSelectedJobTitlesCount(): number {
+    return this.selectedJobTitles.length;
+  }
 
-// Method to get the count of selected employees
-getSelectedEmployeesCount(): number {
-  return this.selectedEmployees.length;
-}
+  // Method to get the count of selected employees
+  getSelectedEmployeesCount(): number {
+    return this.selectedEmployees.length;
+  }
 
-// Method to get the count of selected company names
-getSelectedCompaniesCount(): number {
-  return this.selectedCompanies.length;
-}
-
-
+  // Method to get the count of selected company names
+  getSelectedCompaniesCount(): number {
+    return this.selectedCompanies.length;
+  }
 }
--- project/src/app/filter.pipe.ts
+++ project/src/app/filter.pipe.ts
@@ -4,13 +4,24 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
+
+  // This method transforms the input array of items based on the specified filters.
   transform(items: any[], filters: any): any {
+    // Use the filter method to iterate over each item in the input array.
     return items.filter(item => {
+      // Check if the lowercase version of item's Name includes the lowercase version of searchName.
       const nameMatch = item.Name.toLowerCase().includes(filters.searchName.toLowerCase());
+
+      // Check if selectedJobTitles array is empty or if item's JobTitles is in the selectedJobTitles array.
       const jobTitlesMatch = filters.selectedJobTitles.length === 0 || filters.selectedJobTitles.includes(item.JobTitles);
+
+      // Check if selectedEmployees array is empty or if item's CompanySize is in the selectedEmployees array.
       const employeesMatch = filters.selectedEmployees.length === 0 || filters.selectedEmployees.includes(item.CompanySize);
+
+      // Check if selectedCompanies array is empty or if item's CompanyName is in the selectedCompanies array.
       const companiesMatch = filters.selectedCompanies.length === 0 || filters.selectedCompanies.includes(item.CompanyName);
 
+      // Return true if all filter conditions are met, indicating that the item should be included in the filtered result.
       return nameMatch && jobTitlesMatch && employeesMatch && companiesMatch;
     });
   }
